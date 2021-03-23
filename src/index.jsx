470b96c812a76b328b051a61b730059d1cc2ab73
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-23 16:15:15 +0530

    updated index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './redux/store/index';
 import Routing from './components/Routing';
 import './css/index.css';
 
