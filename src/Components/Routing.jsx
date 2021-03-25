15be502b562e85074962ec239b0fa87e25be817e
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-26 02:37:16 +0530

    made sidebar ui

--- src/Components/Routing.jsx
+++ src/Components/Routing.jsx
@@ -15,52 +15,38 @@ const Routing = () => {
 	return (
 		<BrowserRouter>
 			<Header />
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-md-3 card" style={{ height: '100vh' }}>
-						<SideNav />
-					</div>
-					<div className="col-md-6 card main-container">
-						<Switch>
-							<Route exact path="/" component={HomePage} />
-							<Route
-								exact
-								path="/explore"
-								component={ExplorePage}
-							/>
-							<Route
-								exact
-								path="/quotation"
-								component={ExplorePage}
-							/>
-							<Route exact path="/save" component={ExplorePage} />
-							<Route
-								exact
-								path="/profile"
-								component={ExplorePage}
-							/>
-							<Route
-								exact
-								path="/setting"
-								component={ExplorePage}
-							/>
-							<Route
-								exact
-								path="/artist/:userId"
-								component={ExplorePage}
-							/>
-							<Route exact path="/auth/login" component={Login} />
-							<Route
-								exact
-								path="/auth/register"
-								component={Register}
-							/>
-							<Route component={PageNotFound} />
-						</Switch>
-					</div>
-					<div className="col-md-3 card" style={{ height: '100vh' }}>
-						<RightSide />
-					</div>
+			<div className="container-fluid d-flex flex-row">
+				<div className="rightSideBar">
+					<SideNav />
+				</div>
+				<div className="card main-container">
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route exact path="/explore" component={ExplorePage} />
+						<Route
+							exact
+							path="/quotation"
+							component={ExplorePage}
+						/>
+						<Route exact path="/save" component={ExplorePage} />
+						<Route exact path="/profile" component={ExplorePage} />
+						<Route exact path="/setting" component={ExplorePage} />
+						<Route
+							exact
+							path="/artist/:userId"
+							component={ExplorePage}
+						/>
+						<Route exact path="/auth/login" component={Login} />
+						<Route
+							exact
+							path="/auth/register"
+							component={Register}
+						/>
+						<Route component={PageNotFound} />
+					</Switch>
+				</div>
+				<div className="card leftSideBar" style={{ height: '100vh' }}>
+					<RightSide />
 				</div>
 			</div>
 			<Footer />
