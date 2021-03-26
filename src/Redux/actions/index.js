a7603187e4dd4ca5f77855bab2459190b7f28fa5
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-03-26 22:26:18 +0530

    index file corrected

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -1,3 +1,6 @@
 /* Combining all the action in the main action file */
 export * from "./ArtistProfile";
-export * from './userAuth';
+export * from "./userAuth";
+export * from "./ArtistProfile";
+export * from "./SavedPost";
+export * from "./UserProfile";
